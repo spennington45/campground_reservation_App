26c77e57b78d5de67f8a6a4b63d96d3038f3dd14
Matt Goshorn <matt.goshorn@techelevator.com>
2020-08-19 15:26:29 -0400

    Replaced Menu.java

--- java/src/main/java/com/techelevator/view/Menu.java
+++ java/src/main/java/com/techelevator/view/Menu.java
@@ -1,4 +1,4 @@
-package com.techelevator;
+package com.techelevator.view;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -17,7 +17,7 @@ public class Menu {
 
 	public Object getChoiceFromOptions(Object[] options) {
 		Object choice = null;
-		while(choice == null) {
+		while (choice == null) {
 			displayMenuOptions(options);
 			choice = getChoiceFromUserInput(options);
 		}
@@ -29,27 +29,23 @@ public class Menu {
 		String userInput = in.nextLine();
 		try {
 			int selectedOption = Integer.valueOf(userInput);
-			
-			if(selectedOption > 0 && selectedOption <= options.length) {
+			if (selectedOption > 0 && selectedOption <= options.length) {
 				choice = options[selectedOption - 1];
 			}
-		} catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			// eat the exception, an error message will be displayed below since choice will be null
 		}
-		if(choice == null) {
-			out.println("\n*** "+userInput+" is not a valid option ***\n");
+		if (choice == null) {
+			out.println("\n*** " + userInput + " is not a valid option ***\n");
 		}
 		return choice;
 	}
 
 	private void displayMenuOptions(Object[] options) {
 		out.println();
-		for(int i = 0; i < options.length; i++) {
-			int optionNum = i+1;
-			 if(!options[i].equals("")) {
-				out.println(optionNum+") "+options[i]);	
-			}
-			
+		for (int i = 0; i < options.length; i++) {
+			int optionNum = i + 1;
+			out.println(optionNum + ") " + options[i]);
 		}
 		out.print("\nPlease choose an option >>> ");
 		out.flush();
