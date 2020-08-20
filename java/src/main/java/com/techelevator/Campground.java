e14b72f6457791573f6aa3a7c0718fef03f60c87
Steven Pennington <spennington45@gmail.com>
2020-08-20 10:37:41 -0400

    campground and park objects

---
+++ java/src/main/java/com/techelevator/Campground.java
@@ -0,0 +1,51 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public class Campground {
+
+	private long campgroundIdd;
+	private long parkId;
+	private String name;
+	private String openFrom;
+	private String openTo;
+	private BigDecimal fee;
+	
+	public long getCampgroundIdd() {
+		return campgroundIdd;
+	}
+	public void setCampgroundIdd(long campgroundIdd) {
+		this.campgroundIdd = campgroundIdd;
+	}
+	public long getParkId() {
+		return parkId;
+	}
+	public void setParkId(long parkId) {
+		this.parkId = parkId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOpenFrom() {
+		return openFrom;
+	}
+	public void setOpenFrom(String openFrom) {
+		this.openFrom = openFrom;
+	}
+	public String getOpenTo() {
+		return openTo;
+	}
+	public void setOpenTo(String openTo) {
+		this.openTo = openTo;
+	}
+	public BigDecimal getFee() {
+		return fee;
+	}
+	public void setFee(BigDecimal fee) {
+		this.fee = fee;
+	}
+
+}
--- java/src/main/java/com/techelevator/CampgroundCLI.java
+++ java/src/main/java/com/techelevator/CampgroundCLI.java
@@ -4,14 +4,19 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import com.techelevator.view.Menu;
+
 public class CampgroundCLI {
 
+	private static String [] PARK_OPTIONS;
+	private Menu menu;
+	
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setUrl("jdbc:postgresql://localhost:5432/campground");
 		dataSource.setUsername("postgres");
 		dataSource.setPassword("postgres1");
-
+		Menu menu = new Menu(System.in, System.out);
 		CampgroundCLI application = new CampgroundCLI(dataSource);
 		application.run();
 	}
@@ -21,6 +26,18 @@ public class CampgroundCLI {
 	}
 
 	public void run() {
+		while (true) {
+			String choice = (String) menu.getChoiceFromOptions(PARK_OPTIONS);
 
+			if (choice.equals("")) {
+				
+			} else if (choice.equals("")) {
+				
+			} else if (choice.equals("")) {
+				
+			} else {
+				System.exit(0);
+			}
+		}
 	}
 }
