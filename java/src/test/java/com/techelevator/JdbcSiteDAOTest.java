5f480484e8d54afdf41027ca9658a8bc923a383e
Tim Escondo <tim.escondo@gmail.com>
2020-08-22 13:37:52 -0400

    edited siteDaoTest

--- java/src/test/java/com/techelevator/JdbcSiteDAOTest.java
+++ java/src/test/java/com/techelevator/JdbcSiteDAOTest.java
@@ -19,16 +19,11 @@ import com.techelevator.JdcbSiteDAO;
 public class JdbcSiteDAOTest {
 	private static SingleConnectionDataSource dataSource;
 	private JdcbSiteDAO siteDao;
-	private JDBCCampgroundDAO campDao;
-	private JDBCParkDAO parkDao;
 	private String PName = "'Steve and Tim Park'";
 	private String PLocation = "'State'";
-	private LocalDate estDate = LocalDate.of(1987,01,07);
 	private long PArea = 999999;
 	private long PVisitors = 9999999;
 	private String PDescription = "'This description is only a test'";
-	private long campIdTest;
-	private long siteIdTest;
 	private long parkIdTest;
 	
 	
@@ -57,10 +52,6 @@ public class JdbcSiteDAOTest {
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 		parkIdTest = jdbcTemplate.queryForObject(" INSERT INTO park (name, location, establish_date, area, visitors, description) "
 					+ "VALUES ("+ PName +", "+PLocation+", '1987-01-07', "+PArea+", "+PVisitors+", "+PDescription+") RETURNING park_id", Long.class);			
-		campIdTest = jdbcTemplate.queryForObject("INSERT INTO campground (park_id, name, open_from_mm, open_to_mm, daily_fee) VALUES ("+parkIdTest+", 'Camp Camp', '01', '12', '$22.00') RETURNING campground_id", Long.class);
-		siteIdTest = jdbcTemplate.queryForObject("INSERT INTO site (campground_id, site_number, max_occupancy, accessible, max_rv_length, utilities) VALUES ("+campIdTest+", 999, 3, true, 0, true) RETURNING site_id",  Long.class);
-		parkDao = new JDBCParkDAO(dataSource);
-		campDao = new JDBCCampgroundDAO(dataSource);
 		siteDao = new JdcbSiteDAO(dataSource);
 	
 	}
